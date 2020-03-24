95fcdf2c2f5ff075c14ba13b6e7ade7e307ee1ce
strole <luka.stroliga@gmail.com>
2020-03-24 17:41:11 +0100

    Prvi commit

---
+++ PiS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halo halo metnem ti ga ...");
+            Console.WriteLine("ha ha ha");
+        }
+    }
+}
